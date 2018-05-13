05b882ca5dc474dcc7c5c4f87f53b024c836e834
Samson <samson.zying@gmail.com>
2018-05-13 15:56:53 +0800

    Initial commit

---
+++ src/main/java/oop/fiveonethree/numberguess/NumberGuessApplication.java
@@ -0,0 +1,12 @@
+package oop.fiveonethree.numberguess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberGuessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberGuessApplication.class, args);
+    }
+}
